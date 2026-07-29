357d82619c64c46631b11812a6735c460ad24b93
tnd2x5 <tnd2x5@gmail.com>
2026-07-29 13:25:54 +0700

    Update route.ts

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-const TOKEN = '8284803049:AAFL7WWNmphl917GhqLGInVl8bk082TFH1c';
-const CHAT_ID = '1922578871';
+const TOKEN = '8471670851:AAELVC1osvNho9mdIZhnzRFg9Lt0LMVxQfw';
+const CHAT_ID = '1819276982';
 
 const POST = async (req: NextRequest) => {
     try {
